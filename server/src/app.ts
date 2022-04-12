66e5a7f45d25731d38218f0322f9d61386b3ea42
Cedric Heijlman <90176813+cedricheijlman@users.noreply.github.com>
2022-04-12 20:49:41 +0200

    server setup

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,14 @@
 import express, { Application, Request, Response } from "express";
+import "dotenv/config";
+require("dotenv").config();
 
 const app: Application = express();
+const PORT = process.env.port || 5000;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("hello");
 });
 
-app.listen(5000, () => {
-  console.log("running");
+app.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`);
 });
