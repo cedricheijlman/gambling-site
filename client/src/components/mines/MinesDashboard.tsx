3f7d922183109d12ef1c612f2fef96621fe2f85a
cedricheijlman <90176813+cedricheijlman@users.noreply.github.com>
2022-04-16 20:06:01 +0200

    Mines Total

--- client/src/components/mines/MinesDashboard.tsx
+++ client/src/components/mines/MinesDashboard.tsx
@@ -13,6 +13,21 @@ const MinesDashboard: React.FC = () => {
             <input type="number" />
             <button className="maxButton">Max</button>
           </div>
+          <div className="minesContainer__minesTotal">
+            <p>Mines</p>
+            <select>
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+              <option>6</option>
+              <option>7</option>
+              <option>8</option>
+              <option>9</option>
+              <option>10</option>
+            </select>
+          </div>
           <button className="betButton">Bet</button>
         </div>
         <div className="minesContainer__right">
