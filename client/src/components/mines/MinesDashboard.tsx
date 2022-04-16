eae184b0d28166355da74bf1da723f31ed5094a0
cedricheijlman <90176813+cedricheijlman@users.noreply.github.com>
2022-04-16 16:19:54 +0200

    left mines dashboard

--- client/src/components/mines/MinesDashboard.tsx
+++ client/src/components/mines/MinesDashboard.tsx
@@ -1,8 +1,24 @@
 import React from "react";
 import "./MinesDashboard.css";
+import AttachMoneyIcon from "@material-ui/icons/AttachMoney";
 
 const MinesDashboard: React.FC = () => {
-  return <div>MinesDashboard</div>;
+  return (
+    <div className="minesWrapper">
+      <div className="minesContainer">
+        <div className="minesContainer__left">
+          <p>Bet Amount</p>
+          <div className="minesContainer__betContainer">
+            <AttachMoneyIcon className="moneyIcon" />
+            <input type="number" />
+            <button className="maxButton">Max</button>
+          </div>
+          <button className="betButton">Bet</button>
+        </div>
+        <div className="minesContainer__right">Right</div>
+      </div>
+    </div>
+  );
 };
 
 export default MinesDashboard;
