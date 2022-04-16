295d6c457ce5f33fd395c86c0fd17c9cac71de0d
cedricheijlman <90176813+cedricheijlman@users.noreply.github.com>
2022-04-16 21:24:11 +0200

    changebetamount state

--- client/src/components/mines/MinesDashboard.tsx
+++ client/src/components/mines/MinesDashboard.tsx
@@ -1,27 +1,37 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import "./MinesDashboard.css";
 import AttachMoneyIcon from "@material-ui/icons/AttachMoney";
 
 const MinesDashboard: React.FC = () => {
   const userMoneyExample: number = 500003;
+  const [betAmountValue, setBetAmountValue] = useState("");
 
   const minesTotalRef = useRef<HTMLSelectElement>(null);
 
   const playBoard: number[][] = [
-    [1, 1, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1],
     [0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0],
   ];
 
-  const imageHandling = (row: number, col: number) => {
-    if (playBoard[row][col] == 1) {
-      return <h1>Test</h1>;
+  const changeBetAmountValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (Number(e.target.value) <= userMoneyExample) {
+      console.log("he;;p");
+      console.log(e);
+
+      setBetAmountValue(e.target.value);
     }
   };
 
-  const bombImage: string = "./logo192.png";
+  const imageHandling = (row: number, col: number) => {
+    if (playBoard[row][col] == 1) {
+      return <h1>Lose</h1>;
+    } else {
+      return <h1>Win</h1>;
+    }
+  };
 
   const handleBet = () => {
     if (userMoneyExample > 0) {
@@ -36,7 +46,11 @@ const MinesDashboard: React.FC = () => {
           <p>Bet Amount</p>
           <div className="minesContainer__betContainer">
             <AttachMoneyIcon className="moneyIcon" />
-            <input type="number" />
+            <input
+              type="number"
+              value={betAmountValue}
+              onChange={changeBetAmountValue}
+            />
             <button className="maxButton">Max</button>
           </div>
           <div className="minesContainer__minesTotal">
@@ -62,31 +76,31 @@ const MinesDashboard: React.FC = () => {
           <div className="row">
             <div className="col">
               <div className="theFront">
-                <img src="./basket.png" />
+                <img alt="Basketball" src="./basket.png" />
               </div>
               <div className="theBack">{imageHandling(0, 0)}</div>
             </div>
             <div className="col">
               <div className="theFront">
-                <img src="./basket.png" />
+                <img alt="Basketball" src="./basket.png" />
               </div>
               <div className="theBack">{imageHandling(0, 1)}</div>
             </div>
             <div className="col">
               <div className="theFront">
-                <img src="./basket.png" />
+                <img alt="Basketball" src="./basket.png" />
               </div>
               <div className="theBack">{imageHandling(0, 2)}</div>
             </div>
             <div className="col">
               <div className="theFront">
-                <img src="./basket.png" />
+                <img alt="Basketball" src="./basket.png" />
               </div>
               <div className="theBack">{imageHandling(0, 3)}</div>
             </div>
             <div className="col">
               <div className="theFront">
-                <img src="./basket.png" />
+                <img alt="Basketball" src="./basket.png" />
               </div>
               <div className="theBack">{imageHandling(0, 4)}</div>
             </div>
@@ -95,31 +109,31 @@ const MinesDashboard: React.FC = () => {
           <div className="row">
             <div className="col">
               <div className="theFront">
-                <img src="./basket.png" />
+                <img alt="Basketball" src="./basket.png" />
               </div>
               <div className="theBack">{imageHandling(1, 0)}</div>
             </div>
             <div className="col">
               <div className="theFront">
-                <img src="./basket.png" />
+                <img alt="Basketball" src="./basket.png" />
               </div>
               <div className="theBack">{imageHandling(1, 1)}</div>
             </div>
             <div className="col">
               <div className="theFront">
-                <img src="./basket.png" />
+                <img alt="Basketball" src="./basket.png" />
               </div>
               <div className="theBack">{imageHandling(1, 2)}</div>
             </div>
             <div className="col">
               <div className="theFront">
-                <img src="./basket.png" />
+                <img alt="Basketball" src="./basket.png" />
               </div>
               <div className="theBack">{imageHandling(1, 3)}</div>
             </div>
             <div className="col">
               <div className="theFront">
-                <img src="./basket.png" />
+                <img alt="Basketball" src="./basket.png" />
               </div>
               <div className="theBack">{imageHandling(1, 4)}</div>
             </div>
@@ -128,31 +142,31 @@ const MinesDashboard: React.FC = () => {
           <div className="row">
             <div className="col">
               <div className="theFront">
-                <img src="./basket.png" />
+                <img alt="Basketball" src="./basket.png" />
               </div>
               <div className="theBack">{imageHandling(2, 0)}</div>
             </div>
             <div className="col">
               <div className="theFront">
-                <img src="./basket.png" />
+                <img alt="Basketball" src="./basket.png" />
               </div>
               <div className="theBack">{imageHandling(2, 1)}</div>
             </div>
             <div className="col">
               <div className="theFront">
-                <img src="./basket.png" />
+                <img alt="Basketball" src="./basket.png" />
               </div>
               <div className="theBack">{imageHandling(2, 2)}</div>
             </div>
             <div className="col">
               <div className="theFront">
-                <img src="./basket.png" />
+                <img alt="Basketball" src="./basket.png" />
               </div>
               <div className="theBack">{imageHandling(2, 3)}</div>
             </div>
             <div className="col">
               <div className="theFront">
-                <img src="./basket.png" />
+                <img alt="Basketball" src="./basket.png" />
               </div>
               <div className="theBack">{imageHandling(2, 4)}</div>
             </div>
@@ -161,31 +175,31 @@ const MinesDashboard: React.FC = () => {
           <div className="row">
             <div className="col">
               <div className="theFront">
-                <img src="./basket.png" />
+                <img alt="Basketball" src="./basket.png" />
               </div>
               <div className="theBack">{imageHandling(3, 0)}</div>
             </div>
             <div className="col">
               <div className="theFront">
-                <img src="./basket.png" />
+                <img alt="Basketball" src="./basket.png" />
               </div>
               <div className="theBack">{imageHandling(3, 1)}</div>
             </div>
             <div className="col">
               <div className="theFront">
-                <img src="./basket.png" />
+                <img alt="Basketball" src="./basket.png" />
               </div>
               <div className="theBack">{imageHandling(3, 2)}</div>
             </div>
             <div className="col">
               <div className="theFront">
-                <img src="./basket.png" />
+                <img alt="Basketball" src="./basket.png" />
               </div>
               <div className="theBack">{imageHandling(3, 3)}</div>
             </div>
             <div className="col">
               <div className="theFront">
-                <img src="./basket.png" />
+                <img alt="Basketball" src="./basket.png" />
               </div>
               <div className="theBack">{imageHandling(3, 4)}</div>
             </div>
@@ -194,31 +208,31 @@ const MinesDashboard: React.FC = () => {
           <div className="row">
             <div className="col">
               <div className="theFront">
-                <img src="./basket.png" />
+                <img alt="Basketball" src="./basket.png" />
               </div>
               <div className="theBack">{imageHandling(4, 0)}</div>
             </div>
             <div className="col">
               <div className="theFront">
-                <img src="./basket.png" />
+                <img alt="Basketball" src="./basket.png" />
               </div>
               <div className="theBack">{imageHandling(4, 1)}</div>
             </div>
             <div className="col">
               <div className="theFront">
-                <img src="./basket.png" />
+                <img alt="Basketball" src="./basket.png" />
               </div>
               <div className="theBack">{imageHandling(4, 2)}</div>
             </div>
             <div className="col">
               <div className="theFront">
-                <img src="./basket.png" />
+                <img alt="Basketball" src="./basket.png" />
               </div>
               <div className="theBack">{imageHandling(4, 3)}</div>
             </div>
             <div className="col">
               <div className="theFront">
-                <img src="./basket.png" />
+                <img alt="Basketball" src="./basket.png" />
               </div>
               <div className="theBack">{imageHandling(4, 4)}</div>
             </div>
