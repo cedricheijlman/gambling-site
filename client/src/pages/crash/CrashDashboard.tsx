12194abd22eaca5d121fdf8e685f3fb26ea2f046
Cedric Heijlman <90176813+cedricheijlman@users.noreply.github.com>
2022-04-20 19:02:39 +0200

    line chart

--- client/src/pages/crash/CrashDashboard.tsx
+++ client/src/pages/crash/CrashDashboard.tsx
@@ -1,8 +1,49 @@
 import React from "react";
-const Chart = require("chart.js");
+import { Chart } from "react-chartjs-2";
+import { Line } from "react-chartjs-2";
+import "./CrashDashboard.css";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 const CrashDashboard: React.FC = () => {
-  return <div>CrashDashboard</div>;
+  return (
+    <div className="crashDashboard">
+      <Line
+        width={"100%"}
+        height={"100%"}
+        options={{ maintainAspectRatio: false }}
+        data={{
+          labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+          datasets: [
+            {
+              label: "# of votes",
+              data: [12, 12, 13, 16],
+              backgroundColor: "white",
+              borderColor: "white",
+            },
+          ],
+        }}
+      />
+    </div>
+  );
 };
 
 export default CrashDashboard;
