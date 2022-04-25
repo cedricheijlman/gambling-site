d1748d8c8ef80d4d8748bf56276be5215f18b76b
cedricheijlman <90176813+cedricheijlman@users.noreply.github.com>
2022-04-25 16:35:26 +0200

    redux setup

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,11 +4,15 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
 
 const rootElement: any = document.getElementById("root");
 
 createRoot(rootElement).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
