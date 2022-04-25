99e143bc9bfc522259cc66345c6772d20d865ed3
cedricheijlman <90176813+cedricheijlman@users.noreply.github.com>
2022-04-25 21:18:35 +0200

    verify user controller

--- server/src/controllers/authControllers.ts
+++ server/src/controllers/authControllers.ts
@@ -1,8 +1,21 @@
 import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
 
 const verifyUserToken = async (req: Request, res: Response) => {
   try {
-    res.status(200).json({ message: "worked" });
+    const authHeader = req.headers.authorization;
+
+    if (authHeader) {
+      const token: string | number = authHeader.split(" ")[1];
+
+      jwt.verify(token, String(process.env.SECRET_CODE), (err, user) => {
+        if (err) {
+          return res.status(403).json("Token is not valid");
+        }
+
+        return res.status(200).json(user);
+      });
+    }
   } catch (error) {
     res.status(400).json({ message: "Doesn't work" });
   }
