f1948b59a1d134455f556814d531d449d8e036a7
Cedric Heijlman <90176813+cedricheijlman@users.noreply.github.com>
2022-04-26 12:14:21 +0200

    wallet dashboard

--- client/src/pages/wallet/WalletDashboard.tsx
+++ client/src/pages/wallet/WalletDashboard.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import "./WalletDashboard.css";
 
 const WalletDashboard: React.FC = () => {
-  return <div>WalletDashboard</div>;
+  const userBalance = useSelector((state: any) => state.balance.balance);
+  const userName = useSelector((state: any) => state.currentUser.username);
+
+  return (
+    <div className="walletDashboard">
+      <div className="userWallet">
+        <h2>{userName}'s Wallet</h2>
+        <p>Current Balance</p>
+        <h4>${userBalance}</h4>
+      </div>
+
+      <div className="claimBonus">
+        <button>Claim Welcome Bonus</button>
+      </div>
+    </div>
+  );
 };
 
 export default WalletDashboard;
